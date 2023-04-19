6a70928a3b6ea4f0708ababc26d63cd96b12ccaf
Fabio Lex Castro <fabioalexandercastro@gmail.com>
2023-04-19 06:32:22 -0500

    Update shiny_v1.R

--- shiny_v1.R
+++ shiny_v1.R
@@ -11,7 +11,9 @@ dpto <- st_read('https://raw.githubusercontent.com/fabiolexcastro/tutorials_yout
 popu <- suppressMessages(read_csv('https://raw.githubusercontent.com/fabiolexcastro/tutorials_youtube/master/populationMap/tble/demografia_urbana_rural.csv'))
 popu <- filter(popu, year == 2022, mpio != 'Colombia') %>% mutate(codigo = as.character(codigo))
 rslt <- inner_join(dpto, popu, by = c('MPIO_CCNCT' = 'codigo'))
+st_write(rslt, 'popu_v1.gpkg')
 
+# Server y UI -------------------------------------------------------------
 ui <- fluidPage(
   titlePanel("Mapa Coropletico - Población"),
   sidebarLayout(
@@ -57,3 +59,102 @@ server <- function(input, output) {
 
 shinyApp(ui = ui, server = server)
 
+
+#
+
+ui <- fluidPage(
+  titlePanel("Visualización de información espacial"),
+  sidebarLayout(
+    sidebarPanel(
+      selectInput(inputId = "depto_sel", label = "Seleccionar departamento:",
+                  choices = unique(rslt$DPTO_CNMBR), selected = NULL),
+      selectInput(inputId = "var_sel", label = "Seleccionar variable:",
+                  choices = c("Población urbana" = 'urbana', "Población rural" = 'rural'), selected = NULL)
+    ),
+    mainPanel(
+      leafletOutput("mapa", width = "100%", height = "800px")
+    )
+  )
+)
+
+server <- function(input, output, session) {
+  
+  depto_sel <- reactive({
+    rslt %>% filter(DPTO_CNMBR == input$depto_sel)
+  })
+  
+  output$mapa <- renderLeaflet({
+    leaflet(depto_sel()) %>%
+      addProviderTiles("OpenStreetMap.Mapnik") %>%
+      setView(lng = -79.3889, lat = 43.5387, zoom = 8) %>%
+      addPolygons(fillColor = ~colorQuantile("YlOrRd", get(input$var_sel))(get(input$var_sel)),
+                  fillOpacity = 0.7, color = "#BDBDC3", weight = 1) %>%
+      addLegend(pal = colorQuantile("YlOrRd", get(input$var_sel)), 
+                values = depto_sel()[, get(input$var_sel)], 
+                title = "Población", position = "bottomright")
+  })
+  
+}
+
+shinyApp(ui, server)
+
+##
+pal <- colorNumeric(palette = "YlOrRd", domain = NULL)
+
+ui <- fluidPage(
+  titlePanel("Visualización de información espacial"),
+  sidebarLayout(
+    sidebarPanel(
+      selectInput(inputId = "depto_sel",
+                  label = "Seleccione el departamento:",
+                  choices = unique(rslt$DPTO_CNMBR)),
+      selectInput(inputId = "var_sel",
+                  label = "Seleccione la variable:",
+                  choices = c("Población Urbana" = "urbana",
+                              "Población Rural" = "rural"))
+    ),
+    mainPanel(
+      leafletOutput(outputId = "map", width = "100%", height = "600px")
+    )
+  )
+)
+
+# Define el server
+server <- function(input, output) {
+  
+  # Filtar el shapefile
+  sel_dept <- reactive({
+    rslt[rslt$DPTO_CNMBR == input$depto_sel, ]
+  })
+  
+  # Crear el mapa
+  output$map <- renderLeaflet({
+    # Crear mapa base
+    map <- leaflet() %>%
+      addTiles()
+    
+    # Añadir capa de datos
+    map %>% 
+      addPolygons(data = sel_dept(),
+                  fillColor = ~ pal(sel_dept()[, input$var_sel]),
+                  fillOpacity = 0.7,
+                  color = "#BDBDC3",
+                  weight = 1,
+                  popup = paste0("<strong>Departamento:</strong> ",
+                                 sel_dept()$DPTO_CNMBR,
+                                 "<br>",
+                                 "<strong>Población ",
+                                 ifelse(input$var_sel == "urbana", "Urbana:", "Rural:"),
+                                 "</strong> ",
+                                 sel_dept()[, input$var_sel])) %>%
+      # Añadir leyenda
+      addLegend(position = "bottomright",
+                pal = pal,
+                values = sel_dept()[, input$var_sel],
+                title = "Población",
+                labFormat = labelFormat())
+  })
+}
+
+# Run the app
+shinyApp(ui = ui, server = server)
